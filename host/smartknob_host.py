026333e0390b8c82fd14a5f59408ddcd835f0d62
3anya-T0n1c <74545593+Eanya-Tonic@users.noreply.github.com>
2022-12-10 22:19:03 +0800

    Update smartknob_host.py

--- host/smartknob_host.py
+++ host/smartknob_host.py
@@ -12,6 +12,7 @@ interface = devices.Activate(
     IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
 volume = cast(interface, POINTER(IAudioEndpointVolume))
 
+#使用前请修改串口
 connection = serial.Serial('COM4')
 
 updateTime = 0.1  # number of seconds between each update
@@ -20,7 +21,7 @@ updateTime = 0.1  # number of seconds between each update
 def sendData():
     volumeCurrent = round(volume.GetMasterVolumeLevelScalar()*100 + 1)
     try:
-        recv = connection.readline().decode("utf-8")  # 读出串口数据，数据采用gbk编码
+        recv = connection.readline().decode("utf-8")  # 读出串口数据，数据采用utf编码
         if recv.find("VOLUME:") != -1:
             volumeReceive = ''.join([x for x in recv if x.isdigit()])
             volumeReceive = int(volumeReceive)
